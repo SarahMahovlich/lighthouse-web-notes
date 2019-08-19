824cef4250a6694c6368e0d0f015768bd6b44929
Sarah <sarah.mahov@gmail.com>
2019-08-19 23:18:42 +0000

    completed first element activity

---
+++ Week_1/lotide/head.js
@@ -0,0 +1,17 @@
+const head = function(array) {
+  return array[0];
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☹️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Test Code
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), 1);
