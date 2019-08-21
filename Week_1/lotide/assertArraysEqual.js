7c7b68aa345fbd6e01e9f0a0ab8a3b3a4ff0fd09
Sarah <sarah.mahov@gmail.com>
2019-08-21 02:37:57 +0000

    linting

--- Week_1/lotide/assertArraysEqual.js
+++ Week_1/lotide/assertArraysEqual.js
@@ -14,16 +14,16 @@ const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
-    } 
+    }
   }
-    return true;
+  return true;
 };
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(assertEqual(true, true));
   } else console.log(assertEqual(false, false));
-}
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, 4]);
