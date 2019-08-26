8986dba74f75f52d647078cab1724de08ce06293
Sarah <sarah.mahov@gmail.com>
2019-08-26 16:33:24 +0000

    head function refactored with module.export and separate test file

--- Week_1/lotide/head.js
+++ Week_1/lotide/head.js
@@ -2,16 +2,6 @@ const head = function(array) {
   return array[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`☹️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+module.exports = head;
 
-//Test Code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), 1);
+
