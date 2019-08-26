49c5a09e80577b17ad0421f39f6141bb30060053
Sarah <sarah.mahov@gmail.com>
2019-08-26 19:51:40 +0000

    tail refactored with assert testing in separate file

--- Week_1/lotide/tail.js
+++ Week_1/lotide/tail.js
@@ -3,22 +3,4 @@ const tail = function(array) {
   return tailArr;
 };
 
-//Test Code
-console.log(tail([1]));
-console.log(tail([]));
-
-const assertEqual = function(actual, expected) {
-  if (actual.join() === expected.join()) {
-    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`☹️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//Test Code
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-const result2 = tail([1]);
-assertEqual(result2, [1]);
-
-
+module.exports = tail;
